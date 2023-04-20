bccfa7cbe2aa52e0e5b7e18bc0617bf80f316505
pachasp <elovfdhfshx475@gmail.com>
2023-04-20 10:19:12 -0500

    first commit

---
+++ Lab/Lab/ButtonCode.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ButtonCode : ContentPage
+    {
+        public ButtonCode()
+        {
+            InitializeComponent();
+
+            Title = "Code Button Click";
+
+            Label label = new Label
+            {
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            Button button = new Button
+            {
+                Text = "Clicked to Rotate Text!",
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.Center
+            };
+            button.Clicked += async (sender, args) => await label.RelRotateTo(360, 1000);
+
+            Content = new StackLayout
+            {
+                Children =
+                {
+                    label,
+                    button
+                }
+            };
+        }
+    }
+}
